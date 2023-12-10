96c90eda92f4462d5c61a8f7d2bd8c5693e2c233
saiyia <153315535+saiyia@users.noreply.github.com>
2023-12-10 17:12:00 +0300

    Create Taskk.cs

---
+++ lab2/Model/Taskk.cs
@@ -0,0 +1,9 @@
+namespace Lab2.Model;
+
+public class Taskk
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+    public List<string> Tags { get; set; }
+}

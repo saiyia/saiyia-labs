4633c86c56e8fd9b605d281c8c83e017fb708280
saiyia <153315535+saiyia@users.noreply.github.com>
2024-01-08 23:37:34 +0300

    Add files via upload

---
+++ lab5/Views/DialogWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NastyaLab5.Views;
+
+public partial class DialogWindow : Window
+{
+    public DialogWindow()
+    {
+        InitializeComponent();
+    }
+}
